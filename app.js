2ee68a65577b7b4910526983d297c4e5fa70e853
MrSRK <rubes6@hotmail.com>
2019-09-17 11:30:16 +0300

    travis

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const chalk=require('chalk')
 
 const nvc1=require('./nvc1/nvc1')
 
-nvc1.run(async(error,app)=>
+nvc1.run((error,app)=>
 {
-	if(error)
+	try
+	{
+		if(error)
+			throw(error)
+		return console.log('%s Process Running...',chalk.blue('i'))
+	}
+	catch(error)
 	{
 		console.error(error)
 		console.log('%s Process Exit...',chalk.blue('X'))
 		process.exit()
 	}
-   
-	return console.log('%s Process Running...',chalk.blue('i'))
+	finally
+	{
+		if(process.env.TEST==true)
+			return process.exit()
+	}
 })
